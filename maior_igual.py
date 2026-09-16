0e0593d5eb1c0ffe95f53edaf23d09c73a54765f
Gabriela Queiroz <gabrielaqueiroznunes2501@gmail.com>
2026-09-16 17:32:26 -0300

    Maior ou igual

---
+++ maior_igual.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Digite o primeiro numero:"))
+numero2 = float(input("Digite o segundo numero:"))
+
+if numero1 > numero2:
+    print(f"O primeiro valor ({numero1}) é o maior")
+elif numero2 > numero1:
+    print(f"O segundo numero ({numero2}) é o maior")
+else:
+    print("Os numeros são iguais ")        
